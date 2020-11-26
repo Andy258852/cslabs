d70aad761466be274abe05aa0a86c4cab47d446e
Andy258852 <62135208+Andy258852@users.noreply.github.com>
2020-11-26 18:01:46 +0300

    Create MyLink.cs

---
+++ lab3/Service/MyLink.cs
@@ -0,0 +1,13 @@
+namespace Service1
+{
+    class MyLink
+    {
+        public MyLink(bool Type, string Name)
+        {
+            this.Type = Type;
+            this.Name = Name;
+        }
+        public bool Type { get; set; }
+        public string Name { get; set; }
+    }
+}
